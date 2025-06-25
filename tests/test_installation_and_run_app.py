b78c19b88d01af3e5382b3d47da1536df1605848
sserebrinka <lenaignatyeva77@gmail.com>
2025-06-25 11:40:55 +0300

    add allure

--- tests/test_installation_and_run_app.py
+++ tests/test_installation_and_run_app.py
@@ -1,9 +1,11 @@
 from utils.install import install_app
 from utils.verification import is_app_running
 from utils.launch import wait_for_app_launch, close_app
+import allure
 
 
-def test_installation():
+@allure.title("Запуск теста на установку приложения VK Teams")
+def test_installation_and_run_app():
     install_app()
     assert install_app(), "Установка VK Teams не завершилась"
     print("Установка VK Teams успешно завершена")
